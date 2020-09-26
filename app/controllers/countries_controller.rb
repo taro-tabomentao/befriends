0b7f9528ad9479e48e9b69bf97097ba9e8f6e472
ShotaroOkawa <shotaro.okawa@outlook.com>
2020-09-26 23:17:32 +0900

    Add: メイン部分のフロント実装

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,4 +1,12 @@
 class CountriesController < ApplicationController
   def index
+    @asia_countries = Country.find_all_by_region "Asia"
+    @europe_countries = Country.find_all_by_region "Europe"
+    @north_america_countries = Country.find_all_by_region "North America"
+    @south_america_countries = Country.find_all_by_region "South America"
+    @central_america_countries = Country.find_all_by_region "Central America"
+    @oceania_countries = Country.find_all_by_region "Oceania"
+    @africa_countries = Country.find_all_by_region "Africa"
+    @middle_east_countries = Country.find_all_by_region "Middle East"
   end
 end
