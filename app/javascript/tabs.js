f9f4e879e75c0b6ec32a3e11544e8791a3d8100e
ShotaroOkawa <shotaro.okawa@outlook.com>
2020-09-27 17:39:17 +0900

    Add: タブ毎に表示するページを変更する機能を追加
    タブはArticle、Q&A、Eventのページを持っており、タブをクリックするとそれぞれのページを表示するようにするため

---
+++ app/javascript/tabs.js
@@ -0,0 +1,28 @@
+document.addEventListener("turbolinks:load", () => {
+  console.log('Test')
+  const tabItems = document.querySelectorAll('.tab-item');
+  const tabContentItems = document.querySelectorAll('.tab-content-item');
+  
+  // タブを選択する
+  function selectItem(e) {
+    removeBorder();
+    removeShow();
+    // 選択したタブにボーダーを追加する
+    this.classList.add('tab-border');
+    // 選択したタブの中身を取得する
+    const tabContentItem = document.querySelector(`#${this.id}-content`);
+    // 選択したタブに紐づく中身を表示する
+    tabContentItem.classList.add('show');
+  }
+  
+  function removeBorder(){
+    tabItems.forEach(item => item.classList.remove('tab-border'));
+  }
+  
+  function removeShow() {
+    tabContentItems.forEach(item => item.classList.remove('show'));
+  }
+  
+  // 各タブにメソッドをセットする
+  tabItems.forEach(item => item.addEventListener('click', selectItem));
+})
