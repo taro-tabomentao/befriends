fffd20d1c3aa950b93abe00b37d56f8f83cb5172
ShotaroOkawa <shotaro.okawa@outlook.com>
2020-09-27 12:11:40 +0900

    Add: Genderモデルを追加
    新規登録画面に必要なので、Genderモデルを追加し、関連付け。（Countryモデルも）

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :gender
+  belongs_to_active_hash :country
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :image
