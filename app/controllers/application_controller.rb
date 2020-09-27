d5510db1cc53d0e345328202b159a3e00fe2569c
ShotaroOkawa <shotaro.okawa@outlook.com>
2020-09-27 11:19:44 +0900

    Add: User認証の受け取ることが出来るパラメータを設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    params.require(:user).permit(:nickname,:gender_id, :country_id, :city, :birthday, :language, :image)
+  end
 end
