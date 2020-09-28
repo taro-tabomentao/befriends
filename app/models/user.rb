009fa16028d46ab6c764acfefedb21bcb5f1007f
ShotaroOkawa <shotaro.okawa@outlook.com>
2020-09-28 13:39:24 +0900

    Add: User, Event, UserEventの各モデルのアソシエーションを定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :image
+  has_many :events, through: :user_events
 end
