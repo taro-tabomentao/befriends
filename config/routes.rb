058f3eb5165419fc9ebe9d8487b57df957b7de1b
ShotaroOkawa <shotaro.okawa@outlook.com>
2020-09-29 07:30:54 +0900

    Add: Event参加機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :countries, only: [:index,:show] do
-    resources :events, only: [:new, :create, :show] 
+    resources :events do
+      resources :user_events, only: :create
+    end
   end
   resources :users, only: :show
   root to: "countries#index"
