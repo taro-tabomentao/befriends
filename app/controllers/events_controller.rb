456fecc5ce19d88b94ca95df922f58df2622f5ea
ShotaroOkawa <shotaro.okawa@outlook.com>
2020-09-28 17:36:39 +0900

    イベント新規投稿画面実装

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,21 @@
 class EventsController < ApplicationController
   def new
+    @country = Country.find(params[:country_id])
     @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.valid?
+      @event.save
+     redirect_to country_path(params[:country_id])
+    else
+      render :new
+    end
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:title, :country_id, :category_id, :start_time, :end_time, :online, :place, :content, :image, user_ids:[]).merge(country_id: params[:country_id])
   end
 end
