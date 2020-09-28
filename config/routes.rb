473b835fda9de7e673fe58ad9ba87d5748319f2d
ShotaroOkawa <shotaro.okawa@outlook.com>
2020-09-28 18:01:33 +0900

    Fix: ルーティングエラーを修正
    ルートの変更によるルーティングエラーを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :countries, only: [:index,:show] do
-    resources :events, only: [:new, :create] 
+    resources :events, only: [:new, :create, :show] 
   end
   resources :users, only: :show
   root to: "countries#index"
-
 end
