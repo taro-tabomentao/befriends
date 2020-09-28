f944beab5b615d3c7915665da25e36d3afa30bec
ShotaroOkawa <shotaro.okawa@outlook.com>
2020-09-28 13:24:24 +0900

    Add: イベント新規投稿機能の実装
    国別ページにリンクを作り、そこから新規投稿画面に遷移できるようにルーティング、アクション等設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :countries, only: [:index,:show]
+  resources :countries, only: [:index,:show] do
+    resources :events, only: [:new, :create] 
+  end
   resources :users, only: :show
   root to: "countries#index"
 
