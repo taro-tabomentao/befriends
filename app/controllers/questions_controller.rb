ed60cf0582af5cfac1f81e51bfdb5cbc31282a04
ShotaroOkawa <shotaro.okawa@outlook.com>
2020-09-29 16:24:57 +0900

    Add: Q&A詳細表示機能実装

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -14,9 +14,19 @@ class QuestionsController < ApplicationController
     end
   end
 
+
+  def show
+    @country = Country.find(params[:country_id])
+    @question = Question.find(params[:id])
+  end
+
   private
   def new_question_params
     params.require(:question).permit(:title, :category_id, :text).merge(user_id: current_user.id, country_id: params[:country_id],status_id: 2)
+  end
+
+  def edit_question_params
+    params.require(:question).permit(:title, :category_id, :text, :atatus_id).merge(user_id: current_user.id, country_id: params[:country_id])
   end
 end
 
