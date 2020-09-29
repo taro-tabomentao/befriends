2f8e3e758e6871769ab9a18a78d51e744825e984
ShotaroOkawa <shotaro.okawa@outlook.com>
2020-09-29 10:08:51 +0900

    Add:  記事新規投稿機能実装

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,24 @@
 class ArticlesController < ApplicationController
+
+  def new
+    @country = Country.find(params[:country_id])
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.valid?
+      @article.save
+     redirect_to country_path(params[:country_id])
+    else
+      render :new
+    end
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :category_id, :content, :image).merge(country_id: params[:country_id], user_id: current_user.id)
+  end
+
+
 end
