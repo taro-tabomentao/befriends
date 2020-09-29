14c1d92dfb1eca428e9bc697fe1e2581a4047bd7
ShotaroOkawa <shotaro.okawa@outlook.com>
2020-09-29 10:15:58 +0900

    Add: 記事詳細表示画面実装

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,6 +15,37 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def show
+    @country = Country.find(params[:country_id])
+    @article = Article.find(params[:id])
+  end
+
+  def edit
+    @country = Country.find(params[:country_id])
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @country = Country.find(params[:country_id])
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+     redirect_to country_article_path(@country, @article)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @country = Country.find(params[:country_id])
+    @article = Article.find(params[:id])
+    if @event.destroy
+     redirect_to country_path(@country)
+    else
+      render :show
+    end
+  end
+
+
   private
   def article_params
     params.require(:article).permit(:title, :category_id, :content, :image).merge(country_id: params[:country_id], user_id: current_user.id)
