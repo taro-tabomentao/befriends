1e96a617e2c22996c6f09eee896797a70429b336
ShotaroOkawa <shotaro.okawa@outlook.com>
2020-09-29 16:15:21 +0900

    Add: Q&A新規投稿機能実装

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,23 @@
+class QuestionsController < ApplicationController
+  def new
+    @country = Country.find(params[:country_id])
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(new_question_params)
+    if @question.valid?
+      @question.save
+     redirect_to country_path(params[:country_id])
+    else
+      render :new
+    end
+  end
+
+  private
+  def new_question_params
+    params.require(:question).permit(:title, :category_id, :text).merge(user_id: current_user.id, country_id: params[:country_id],status_id: 2)
+  end
+end
+
+
