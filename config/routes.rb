a3a17f3198838f4c124787e70b501765e81d4080
ShotaroOkawa <shotaro.okawa@outlook.com>
2020-09-30 12:51:05 +0900

    Fix: Eventコントローラーの新規投稿リンクをマイページへのリンクへ変更
    新規登録はマイページからの投稿のみとするため

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :countries, only: [:index,:show] do
-    resources :events do
-      resources :user_events, only: :create
-    end
     resources :articles
     resources :questions do
       resources :answers, only: [:new, :create]
     end
+  end
+  resources :events do
+    resources :user_events, only: :create
   end
   resources :users, only: :show
   root to: "countries#index"
