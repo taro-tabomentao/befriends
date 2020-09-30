95c1852bb5e8b986e9dbe0365308ae61c82538ad
ShotaroOkawa <shotaro.okawa@outlook.com>
2020-09-30 14:08:14 +0900

    Fix: Q&A, コメント関連のルーティング変更
    新規登録はマイページからの投稿のみとするため

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,5 @@
 class QuestionsController < ApplicationController
   def new
-    @country = Country.find(params[:country_id])
     @question = Question.new
   end
 
@@ -8,50 +7,46 @@ class QuestionsController < ApplicationController
     @question = Question.new(new_question_params)
     if @question.valid?
       @question.save
-     redirect_to country_path(params[:country_id])
+     redirect_to user_path(current_user)
     else
       render :new
     end
   end
 
   def edit
-    @country = Country.find(params[:country_id])
     @question = Question.find(params[:id])
   end
 
   def update
-    @country = Country.find(params[:country_id])
     @question = Question.find(params[:id])
     if @question.update(edit_question_params)
-      redirect_to country_question_path(@country, @question)
+      redirect_to question_path(@question)
     else
       render :edit
     end
   end
 
   def destroy
-    @country = Country.find(params[:country_id])
     @question = Question.find(params[:id])
     if @question.destroy
-      redirect_to country_path(@country)
+      redirect_to user_path(current_user)
     else
       render :show
     end
   end
 
   def show
-    @country = Country.find(params[:country_id])
     @question = Question.find(params[:id])
     @answers = Answer.where(question_id: params[:id]).order(id: "desc")
   end
 
   private
   def new_question_params
-    params.require(:question).permit(:title, :category_id, :text).merge(user_id: current_user.id, country_id: params[:country_id],status_id: 2)
+    params.require(:question).permit(:title, :category_id, :country_id, :text).merge(user_id: current_user.id, status_id: 2)
   end
 
   def edit_question_params
-    params.require(:question).permit(:title, :category_id, :text, :atatus_id).merge(user_id: current_user.id, country_id: params[:country_id])
+    params.require(:question).permit(:title, :category_id, :country_id, :text, :status_id).merge(user_id: current_user.id)
   end
 end
 
