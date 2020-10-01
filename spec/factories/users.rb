ae21e494486abc0ebbed0cb30048b434d94043b7
ShotaroOkawa <shotaro.okawa@outlook.com>
2020-10-01 10:53:22 +0900

    Add: UserモデルのFactoryBot作成

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    full_name                   { Faker::Name }
+    email                       { Faker::Internet.free_email }
+    password                    { Faker::Internet.password(min_length: 6, mix_case: true) }
+    password_confirmation       {password}
+    gender_id                   { Faker::Number.between(from: 2, to: 4) }
+    country_id                  { Faker::Number.between(from: 2, to: 62) }
+    city                        { Faker::Address.city }
+    birthday                    { Faker::Date.birthday }
+    language                    { "Japanese, English" }
+  end
+end
