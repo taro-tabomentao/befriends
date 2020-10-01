77ff9e14800865b924378c7687558584da37189e
ShotaroOkawa <shotaro.okawa@outlook.com>
2020-10-01 09:45:20 +0900

    Add: イベントモデルのバリデーション追加

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,4 +5,19 @@ class Event < ApplicationRecord
   has_one_attached :image  
   has_many :user_events
   has_many :users, through: :user_events, dependent: :destroy
+
+  with_options numericality: { other_than: 1 } do
+    validates :country_id
+    validates :category_id
+  end
+
+  with_options presence: true do
+    validates :title
+    validates :start_time
+    validates :end_time
+    validates :online
+    validates :place
+    validates :content
+    validates :image
+  end
 end
