85bc981075de8fe2ba4be2b2cc3cb99c7db44752
ShotaroOkawa <shotaro.okawa@outlook.com>
2020-10-03 21:27:22 +0900

    Add: passwordのバリデーションを変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,4 +24,8 @@ class User < ApplicationRecord
     validates :city
     validates :birthday
   end
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'includes both letters and numbers'
+  
 end
