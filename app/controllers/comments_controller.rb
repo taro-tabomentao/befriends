e674cc3fa20646872f8136d82f695d1a62773448
ShotaroOkawa <shotaro.okawa@outlook.com>
2020-10-03 20:54:23 +0900

    Add: コメントコントローラー作成

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,23 @@
+class CommentsController < ApplicationController
+  def new
+    @article = Question.find(params[:article_id])
+    @comment = Comment.new
+  end
+
+  def create
+    @article = Article.find(params[:article_id])
+    @comment = Comment.new(comment_params)
+    if @comment.valid?
+      @comment.save
+      redirect_to article_path(@article)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content).merge(user_id: current_user.id, article_id: params[:article_id])
+  end
+end
