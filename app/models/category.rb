ca76f722b1ad00b945d8c00adf11d4a112ec5c66
ShotaroOkawa <shotaro.okawa@outlook.com>
2020-10-03 14:23:02 +0900

    Add: カテゴリーの項目追加 Fixed #11

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,16 @@
 class Category < ActiveHash::Base
   self.data = [
-    { id: 1, name: '--' },
-    { id: 2, name: 'Travel' },
-    { id: 3, name: 'Food/Restaurant' },
-    { id: 4, name: 'General' }
+    { id: 1,  name: '--' },
+    { id: 2,  name: 'General' },
+    { id: 3,  name: 'Everyday Life' },
+    { id: 4,  name: 'Business' },
+    { id: 5,  name: 'Culture' },
+    { id: 6,  name: 'Travel' },
+    { id: 7,  name: 'Food & Drink' },
+    { id: 8,  name: 'Sports' },
+    { id: 9,  name: 'Music' },
+    { id: 10, name: 'Shopping' },
+    { id: 11, name: 'Anime/Manga' },
+    { id: 12, name: 'Others' },
   ]
 end
