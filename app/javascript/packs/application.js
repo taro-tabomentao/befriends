0765068cdbc1a56843280775320e020901402bd6
ShotaroOkawa <shotaro.okawa@outlook.com>
2020-10-08 11:49:46 +0900

    Add: Slick導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,10 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require('jquery')
 require("../tabs")
+require("../carousel")
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
