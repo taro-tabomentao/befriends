7df2c1ed6230a61a13dc173bea6aa0c3365f914c
ShotaroOkawa <shotaro.okawa@outlook.com>
2020-10-08 13:27:12 +0900

    Add: 記事タブの中身にカルーセルを実装

--- app/javascript/carousel.js
+++ app/javascript/carousel.js
@@ -1,9 +1,14 @@
 $(document).on('turbolinks:load', function() { 
-  $('.list-event').slick({
+  var slider = $('.slider').slick({
     prevArrow: '<i class="fas fa-caret-square-left"></i>',
     nextArrow: '<i class="fas fa-caret-square-right"></i>',
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 1
+  });
+  $(".tab-item").each(function(index) {
+    $(this).on("click", function(){
+      slider.slick('setPosition');
+    })
   });
 });
