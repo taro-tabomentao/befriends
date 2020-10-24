b5dc269bbceba050bd8fc31eb53b1a87f904848a
ShotaroOkawa <shotaro.okawa@outlook.com>
2020-10-24 12:42:18 +0900

    Add： ゲストログインのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
   resources :countries, only: [:index,:show]
   resources :events do
     resources :user_events, only: :create
@@ -21,5 +24,6 @@ Rails.application.routes.draw do
     end
   end
   resources :users, only: [:show, :edit, :update]
+  　
   root to: "countries#index"
 end
