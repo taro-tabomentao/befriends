4a56dade9fe64abdab94e5bdd5022262d8af80f8
ShotaroOkawa <shotaro.okawa@outlook.com>
2020-10-24 14:04:38 +0900

    Improve: Rubocop実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,10 +25,10 @@ class User < ApplicationRecord
   end
 
   def self.guest
-    find_or_initialize_by(full_name: 'Guest', email: 'guest@example.com', gender_id: 2, country_id: 32, city: "Tokyo", birthday: "1990-3-16",language: "Japanese, English" ) do |user|
+    find_or_initialize_by(full_name: 'Guest', email: 'guest@example.com', gender_id: 2, country_id: 32, city: 'Tokyo', birthday: '1990-3-16', language: 'Japanese, English') do |user|
       user.password = SecureRandom.urlsafe_base64
       user.save
-      user.image.attach(io: File.open("./db/fixtures/icon10.jpg"), filename: "icon10.jpg" )
+      user.image.attach(io: File.open('./db/fixtures/icon10.jpg'), filename: 'icon10.jpg')
     end
   end
 
